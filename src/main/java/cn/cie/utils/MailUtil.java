033291d97e70b74d66f203b14a7d9d95337a895a
zhugeyuanzhang <13023329028@163.com>
2019-04-24 12:32:59 +0800

    邮箱及授权码修改

--- src/main/java/cn/cie/utils/MailUtil.java
+++ src/main/java/cn/cie/utils/MailUtil.java
@@ -9,8 +9,8 @@ import org.apache.commons.mail.SimpleEmail;
  */
 public class MailUtil {
 
-    private final static String USERNAME = "rojeralone@163.com";
-    private final static String PASSWORD = "password";           // 需要打开SMTP并使用此授权码登录
+    private final static String USERNAME = "wojiaozhuyuanzhang@163.com";
+    private final static String PASSWORD = "zaq123edc";           // 需要打开SMTP并使用此授权码登录
 
     /**
      * 发送邮件给指定人，需要主题和内容
